23bdff1cbd712f6bcc74d233b8e93551bc46eab1
rkolyan <blendervik@gmail.com>
2025-02-22 14:04:43 +0300

    Finish app

---
+++ app/send.c
@@ -0,0 +1,39 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "ps_syscall.h"
+
+int flag = 1;
+
+void handler(int signum) {
+	if (signum == SIGINT)
+		flag = 0;
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		puts("Неверный ввод команды, введите:\n./send.out <номер узла>");
+		return 0;
+	}
+	signal(SIGINT, handler);
+	unsigned long id = strtoul(argv[1], NULL, 10);
+	int err = ps_node_publish(id);
+	printf("ps_node_publish:err = %d\n", err);
+	if (err) {
+		puts("id not found");
+		return 0;
+	}
+	char buf[11] = {'\0'};
+	srand(time(NULL));
+	while (flag) {
+		sprintf(buf, "%10d", rand());
+		err = ps_node_send(id, buf);
+		printf("ps_node_send:err = %d\tbuf = %10s\n", err, buf);
+		sleep(1);
+	}
+	err = ps_node_unpublish(id);
+	printf("ps_node_publish:err = %d\n", err);
+	return 0;
+}
